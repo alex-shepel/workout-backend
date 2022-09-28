094e1de89988ed920021a8061a08b0a3fcb4df9e
o.shepel <o.shepel@abtollc.com>
2022-09-28 10:51:04 +0300

    remove deprecated api/templates controller and correspondent service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ExercisesGroupsModule } from './exercises-groups/exercises-groups.module';
 import { ExercisesGroupModel } from './exercises-groups/exercises-groups.model';
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
   imports: [
     ConfigModule.forRoot({
       envFilePath: `.env.${process.env.NODE_ENV}`,
